d85ced0c0ebc1c34d00db38df721f903e5e370d4
TayGov <taylor.clausen@gov.bc.ca>
2021-03-12 07:22:08 -0800

    Its a me mario

---
+++ src/jag-mail-it/src/main/java/ca/bc/gov/open/jag/jagmailit/JagMailItApplication.java
@@ -0,0 +1,13 @@
+package ca.bc.gov.open.jag.jagmailit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JagMailItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JagMailItApplication.class, args);
+	}
+
+}
---
+++ src/jag-mail-it/src/test/java/ca/bc/gov/open/jag/jagmailit/JagMailItApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.bc.gov.open.jag.jagmailit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JagMailItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

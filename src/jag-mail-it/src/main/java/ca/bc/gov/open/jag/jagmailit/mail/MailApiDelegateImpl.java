e4ede49d4ab07329b04ec138006e848489f9fbd1
Brendan Beach <55457785+BrendanBeachBC@users.noreply.github.com>
2021-05-18 12:37:36 -0700

    Added logging messages (#26)

--- src/jag-mail-it/src/main/java/ca/bc/gov/open/jag/jagmailit/mail/MailApiDelegateImpl.java
+++ src/jag-mail-it/src/main/java/ca/bc/gov/open/jag/jagmailit/mail/MailApiDelegateImpl.java
@@ -5,6 +5,8 @@ import ca.bc.gov.open.jag.jagmailit.api.model.EmailObject;
 import ca.bc.gov.open.jag.jagmailit.api.model.EmailRequest;
 import ca.bc.gov.open.jag.jagmailit.api.model.EmailResponse;
 import ca.bc.gov.open.jag.jagmailit.mail.mappers.SimpleMessageMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.SimpleMailMessage;
@@ -20,6 +22,8 @@ public class MailApiDelegateImpl implements MailApiDelegate {
 
     private final SimpleMessageMapper simpleMessageMapper;
 
+    Logger logger = LoggerFactory.getLogger(MailApiDelegateImpl.class);
+
     public MailApiDelegateImpl(JavaMailSender emailSender, SimpleMessageMapper simpleMessageMapper) {
         this.emailSender = emailSender;
         this.simpleMessageMapper = simpleMessageMapper;
@@ -27,20 +31,24 @@ public class MailApiDelegateImpl implements MailApiDelegate {
 
     @Override
     public ResponseEntity<EmailResponse> mailSend(EmailRequest emailRequest) {
-
+        logger.info("Beginning mail send");
         Optional<EmailObject> emailObject = emailRequest.getTo().stream().findFirst();
 
         if (!emailObject.isPresent()) {
+            logger.error("No value present in email object");
             return new ResponseEntity("error", HttpStatus.BAD_REQUEST);
         }
 
+        logger.info("Mapping message");
         SimpleMailMessage simpleMailMessage = simpleMessageMapper.toSimpleMailMessage(emailRequest);
 
+        logger.info("Sending message");
         emailSender.send(simpleMailMessage);
 
         EmailResponse emailResponse = new EmailResponse();
         emailResponse.setAcknowledge(true);
 
+        logger.info("Message sent successfully");
         return ResponseEntity.accepted().body(emailResponse);
 
     }
